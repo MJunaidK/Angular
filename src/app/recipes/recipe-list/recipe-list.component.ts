c9d1839d05e0cd7e6c7ebf20c23095aa76090ad4
MJunaidK <junaidcts@gmail.com>
2020-09-02 15:09:11 -0400

    Programmatic navigation to the edit page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe } from '../recipe.model'  
 import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,10 +15,16 @@ export class RecipeListComponent implements OnInit {
 
 
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+                private router: Router,
+                private route: ActivatedRoute  ) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route})
   }
 
     
