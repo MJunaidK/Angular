f19333821d2e4b497788178ddc1625ef8639324c
MJunaidK <junaidcts@gmail.com>
2020-09-07 10:36:45 -0400

    Improving the reactive service with Observables (subjects)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,12 @@
 import {Recipe} from './recipe.model';
-import { EventEmitter, Output, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
 
-  @Output() recipeSelected = new EventEmitter<Recipe>();
+  
    private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test',
          'https://media-cdn.tripadvisor.com/media/photo-s/15/06/7b/64/india-non-veg-thali.jpg'
