35d741c0d6e4da0786f305ee29ed3460eb272da5
MJunaidK <junaidcts@gmail.com>
2020-09-15 17:19:20 -0400

    Resolving data before load

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Recipe} from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -21,17 +21,17 @@ export class DataStorageService{
     }
 
     fetchRecipes(){
-        this.http.get<Recipe[]>('GET_URL')
+      return this.http.get<Recipe[]>('GET_URL')
         .pipe(map(recipes =>{
             return recipes.map(recipe => {
                 return  {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []};
             });
-        }))
-        .subscribe(
-            (recipes) => {
-                this.recipesService.setRecipes(recipes);
-            }
-        );
+        }),
+        tap(recipes => {
+            this.recipesService.setRecipes(recipes);
+        })
+        )
+       
     }
 
 }
