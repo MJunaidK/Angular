37f5411f42b3c46a9cd89cb3993ceab3651e617e
MJunaidK <junaidcts@gmail.com>
2020-09-20 13:39:18 -0400

    Preparing programmatic component creation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AlertComponent } from '../shared/alert/alert.component';
 import { AuthResponseData, AuthService } from './auth.service';
 @Component({
     selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent{
     isLoading =false;
     error: string = null;
 
-    constructor(private authService: AuthService, private router: Router){}
+    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver){}
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode ;
@@ -49,9 +50,16 @@ export class AuthComponent{
             },
             errorMessage => { 
                 this.error = errorMessage; 
-                this.isLoading =false; }
+                this.isLoading =false; 
+                this.showErrorAlert(errorMessage);
+            }   
         ); 
         
         form.reset();
+    }
+
+    private showErrorAlert(message: string){
+        // const alertCmp = new AlertComponent();
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
     }
 }
