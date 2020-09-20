16ca7ca42bc247048c7d4b9e27eca2886e5e5dd0
MJunaidK <junaidcts@gmail.com>
2020-09-20 12:08:55 -0400

    Using ngIf for clsoing the modal

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -19,6 +19,10 @@ export class AuthComponent{
         this.isLoginMode = !this.isLoginMode ;
     }
 
+    onHandleError(){
+        this.error = null;
+    }
+
     onSubmit(form : NgForm){
 
         const email = form.value.email;
