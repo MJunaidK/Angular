fdbda70cf6e4a6a514b26e9558270c6a900fa9e7
MJunaidK <junaidcts@gmail.com>
2020-09-19 21:32:32 -0400

    Adding auto logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,6 +23,8 @@ export interface AuthResponseData{
 
      user = new BehaviorSubject<User>(null);
 
+     private tokenExpirationTime:any;
+
     constructor(private http: HttpClient, private router: Router){
 
     }
@@ -58,6 +60,11 @@ export interface AuthResponseData{
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTime){
+            clearTimeout(this.tokenExpirationTime);
+        }
+        this.tokenExpirationTime =null;
     }
 
     autoLogin(){
@@ -68,7 +75,15 @@ export interface AuthResponseData{
         const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
         if(loadedUser.token){
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
         }
+    }
+
+    autoLogout(expirationDuration: number){
+       this.tokenExpirationTime =  setTimeout( () =>{
+            this.logout()
+        }, expirationDuration)
     }
 
 
@@ -76,6 +91,7 @@ export interface AuthResponseData{
         const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000); 
         const user =  new User(email, userId, token,expirationDate);
         this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 
