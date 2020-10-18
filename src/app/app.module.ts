46f177ae9c906d52bdb01304001dcf647ba4c225
MJunaidK <junaidcts@gmail.com>
2020-10-17 21:55:36 -0400

    Setting uo ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
+import { StoreModule} from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,7 @@ import { LoggingService } from './logging.service';
     BrowserModule,
     ReactiveFormsModule ,
     HttpClientModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     AppRoutingModule,
     SharedModule,
     CoreModule
