c8ac2649505eb37836edd25b73939c01b552ab86
MJunaidK <junaidcts@gmail.com>
2020-10-19 21:29:43 -0400

    dispatching Auth action

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,9 @@ import { BehaviorSubject, Observable, of, Subject, throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
+import * as fromApp from '../store/app.reducer'
+import * as AuthActions from './store/auth.action'
+import { Store } from '@ngrx/store';
 
 
 export interface AuthResponseData{
@@ -26,7 +29,7 @@ export interface AuthResponseData{
 
      private tokenExpirationTime:any;
 
-    constructor(private http: HttpClient, private router: Router){
+    constructor(private http: HttpClient, private router: Router,private store:Store<fromApp.AppState>){
 
     }
     signUp(email: string, password: string ){
@@ -60,7 +63,8 @@ export interface AuthResponseData{
     }
 
     logout(){
-        this.user.next(null);
+        //this.user.next(null);
+        this.store.dispatch(new AuthActions.Logout());
         this.router.navigate(['/auth']);
         localStorage.removeItem('userData');
         if(this.tokenExpirationTime){
@@ -76,7 +80,13 @@ export interface AuthResponseData{
         }
         const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
         if(loadedUser.token){
-            this.user.next(loadedUser);
+            //this.user.next(loadedUser);
+            this.store.dispatch(new AuthActions.Login({
+              email:  loadedUser.email,
+              userId: loadedUser.id,
+              token: loadedUser.token,
+              expirationDate:   new Date(userData._tokenExpirationDate)
+            }))
             const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
             this.autoLogout(expirationDuration);
         }
@@ -92,7 +102,13 @@ export interface AuthResponseData{
     private handleAuthentication(email: string, userId: string,token: string, expiresIn: number){
         const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000); 
         const user =  new User(email, userId, token,expirationDate);
-        this.user.next(user);
+       // this.user.next(user);
+       this.store.dispatch(new AuthActions.Login({
+        email:  email,
+        userId: userId,
+        token:  token,
+        expirationDate:  expirationDate
+      }))
         this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
